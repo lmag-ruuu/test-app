522839d3eb7fa6999c72f2bfa51f6fa93ceed181
lmagruu <magg4ms@gmail.com>
2023-05-21 12:13:33 -0400

    feat: add holaxd

--- src/components/button/button.component.tsx
+++ src/components/button/button.component.tsx
@@ -3,6 +3,7 @@ import SButton from "./styled/button.styled";
 import { Theme } from "../../theme";
 
 const CButton = ({ children, handleFn }) => {
+  const hola = "xd"
   return (
     <SButton theme={Theme} data-testid="button" onClick={() => handleFn()}>
       {children}
